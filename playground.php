bff1e65fb5a54902281037a1ab3de8c495e52766
tushar-me <tusharimran.dev@gmail.com>
2025-04-13 11:20:41 +0600

    creating and updating a json record

--- playground.php
+++ playground.php
@@ -1,2 +1,15 @@
 <?php
-echo('PHP Playground');
+$data = [
+    'name' => 'Tushar',
+    'age' => '23',
+    'skills' => ['PHP','Express','Nuxt']
+];
+
+file_put_contents('users.json', json_encode($data, JSON_PRETTY_PRINT));
+
+$data = json_decode(file_get_contents('users.json'), true);
+
+$data['name'] = "Tushar Imran Al Manun";
+
+file_put_contents('users.json', json_encode($data, JSON_PRETTY_PRINT));
+
